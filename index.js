cdacdca2815f64ff6703b802c3c45be1d7bb2037
Samuel Ng'ang'a <166153002+devbysamcloudy@users.noreply.github.com>
2025-11-24 11:24:35 +0300

    Refactor temperature calculations and variables

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+let day1 = 68;
+let day2 = 21;
+let day3 = 60;
+let day4 = 80;
+let day5 = 32;
+let day6 = 76;
+let day7 = 22;
+let day8 = 97;
+let day9 = 14;
+let day10 = 43;
+let day11 = 55;
+let day12 = 11;
+let day13 = 90;
+let day14 = 15;
+let day15 = 49;
+let day16 = 73;
+let day17 = 38;
+let day18 = 27;
+let day19 = 84;
+let day20 = 16;
+let day21 = 63;
+let day22 = 39;
+let day23 = 71;
+let day24 = 58;
+let day25 = 47;
+let day26 = 34;
+let day27 = 88;
+let day28 = 19;
+let day29 = 95;
+let day30 = 25;
+let day31 = 52;
+
+let day1C = (day1 - 32) * 5 / 9;
+let day2C = 21;
+let day3C = (day3 - 32) * 5 / 9;
+let day4C = (day4 - 32) * 5 / 9;
+let day5C = 32;
+let day6C = (day6 - 32) * 5 / 9;
+let day7C = 22;
+let day8C = (day8 - 32) * 5 / 9;
+let day9C = 11;
+let day10C = (day10 - 32) * 5 / 9;
+let day11C = (day11 - 32) * 5 / 9;
+let day12C = 14;
+let day13C = (day13 - 32) * 5 / 9;
+let day14C = (day14 - 32) * 5 / 9;
+let day15C = 15;
+let day16C = (day16 - 32) * 5 / 9;
+let day17C = (day17 - 32) * 5 / 9;
+let day18C = (day18 - 32) * 5 / 9;
+let day19C = (day19 - 32) * 5 / 9;
+let day20C = 16;
+let day21C = (day21 - 32) * 5 / 9;
+let day22C = (day22 - 32) * 5 / 9;
+let day23C = (day23 - 32) * 5 / 9;
+let day24C = (day24 - 32) * 5 / 9;
+let day25C = (day25 - 32) * 5 / 9;
+let day26C = (day26 - 32) * 5 / 9;
+let day27C = (day27 - 32) * 5 / 9;
+let day28C = 19;
+let day29C = (day29 - 32) * 5 / 9;
+let day30C = 25;
+let day31C = (day31 - 32) * 5 / 9;
+
+let total = day1C + day2C + day3C + day4C
+ + day5C + day6C + day7C + day8C + day9C
+  + day10C + day11C + day12C + day13C + 
+  day14C + day15C + day16C + day17C + 
+  day18C + day19C + day20C + day21C + 
+  day22C + day23C + day24C + day25C + 
+  day26C + day27C + day28C + day29C + 
+  day30C + day31C;
+
+let average = total / 31;
+
+console.log("Average temperature in Celsius:", average);
 
 
-//! Start the calculation of the total temperatures
-//* Then apply the conversion to calculate the total in the other unit of measurement
-//* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
-
-//! Start the calculation of the average temperatures
-//* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
-
-//! Console.log the results for your own inspection if you'd like
-
-//! After creating the four variables mentioned above, uncomment the following lines
-//* This way you can export them to the test file, this is essential for the tests to work
-
-module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
-};
